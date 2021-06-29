8a93a042d5b9d97c3e21a45377177ad4d91bf010
Davi Fernando Rosa <davir0514@gmail.com>
2021-06-28 23:08:56 -0300

    Arquivo

---
+++ Matriz.java
@@ -0,0 +1,33 @@
+
+
+/**
+ *Podemos fazer uso de
+operadores lógicos "&&" e "||". Quando usamos o operador &&, caso a primeira
+condição avaliada em um comando if seja falsa, automaticamente a segunda
+expressão não será avaliada. Quando usamos ||, caso a primeira condição avaliada
+em um comando if seja verdadeira, automaticamente a segunda expressão
+não será avaliada.
+ *
+ */
+public class Matrizes {
+    
+    
+
+	public static void main(String[] args) {
+	
+		double [][]notasAlunos = new double [30][4];
+		
+		notasAlunos [0][0] = 10;
+		notasAlunos [0][1] = 7;
+		notasAlunos [0][2] = 9;
+		notasAlunos [0][3] = 9.5;
+		
+		notasAlunos [1][0] = 10;
+		notasAlunos [1][1] = 7;
+		notasAlunos [1][2] = 9;
+		notasAlunos [1][3] = 9.3;
+	}
+
+}
+    
+  
